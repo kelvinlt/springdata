a58977667144027ccbc8f6d718109d7a4ad5eb31
DAM <DAM@iMac-34.local>
2017-09-15 10:10:01 +0200

    Initial commit

---
+++ src/main/java/com/example/springdata/SpringdataApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataApplication.class, args);
+	}
+}
