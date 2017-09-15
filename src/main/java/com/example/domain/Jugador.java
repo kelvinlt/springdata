956f07b69e5b8fda7a7e124fb7e25453125bbe20
Kelvin <Kelvinleethen@gmail.com>
2017-09-15 11:01:55 +0200

    Creaciond de posicion y jugador

--- src/main/java/com/example/domain/Jugador.java
+++ src/main/java/com/example/domain/Jugador.java
@@ -1,4 +1,22 @@
 package com.example.domain;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import com.example.domain.Posicion;
+
+@Entity
 public class Jugador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private LocalDate birthday;
+    private Integer numCanastasTotal;
+    private Integer numAsistenciasTotal;
+    private Integer numRebotesTotal;
+    private Posicion posicion;
+
+
+
 }
---
+++ src/main/java/com/example/domain/Posicion.java
@@ -0,0 +1,5 @@
+package com.example.domain;
+
+public enum Posicion {
+    Alero,Base,Pivot,Ala
+}
