a654c4fc7f6f36f043dca6fd4f40ad29c0bfbbaa
Kelvin <Kelvinleethen@gmail.com>
2017-09-19 12:22:31 +0200

    long -> Long Creacion de Services

--- src/main/java/com/example/domain/Equipo.java
+++ src/main/java/com/example/domain/Equipo.java
@@ -1,4 +1,65 @@
 package com.example.domain;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Equipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String name;
+    private String localidad;
+    private LocalDate creacion;
+
+    public Equipo() {
+    }
+
+    public Equipo(String name, String localidad, LocalDate creacion) {
+        this.name = name;
+        this.localidad = localidad;
+        this.creacion = creacion;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public LocalDate getCreacion() {
+        return creacion;
+    }
+
+    public void setCreacion(LocalDate creacion) {
+        this.creacion = creacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", localidad='" + localidad + '\'' +
+                ", creacion=" + creacion +
+                '}';
+    }
 }
--- src/main/java/com/example/domain/Jugador.java
+++ src/main/java/com/example/domain/Jugador.java
@@ -9,7 +9,7 @@ public class Jugador {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String name;
     private LocalDate birthday;
     private Integer numCanastasTotal;
@@ -17,6 +17,84 @@ public class Jugador {
     private Integer numRebotesTotal;
     private Posicion posicion;
 
+    public Jugador() {
+    }
 
+    public Jugador(String name, LocalDate birthday, Integer numCanastasTotal, Integer numAsistenciasTotal, Integer numRebotesTotal, Posicion posicion) {
+        this.name = name;
+        this.birthday = birthday;
+        this.numCanastasTotal = numCanastasTotal;
+        this.numAsistenciasTotal = numAsistenciasTotal;
+        this.numRebotesTotal = numRebotesTotal;
+        this.posicion = posicion;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getNumCanastasTotal() {
+        return numCanastasTotal;
+    }
+
+    public void setNumCanastasTotal(Integer numCanastasTotal) {
+        this.numCanastasTotal = numCanastasTotal;
+    }
+
+    public Integer getNumAsistenciasTotal() {
+        return numAsistenciasTotal;
+    }
+
+    public void setNumAsistenciasTotal(Integer numAsistenciasTotal) {
+        this.numAsistenciasTotal = numAsistenciasTotal;
+    }
+
+    public Integer getNumRebotesTotal() {
+        return numRebotesTotal;
+    }
+
+    public void setNumRebotesTotal(Integer numRebotesTotal) {
+        this.numRebotesTotal = numRebotesTotal;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", numCanastasTotal=" + numCanastasTotal +
+                ", numAsistenciasTotal=" + numAsistenciasTotal +
+                ", numRebotesTotal=" + numRebotesTotal +
+                ", posicion=" + posicion +
+                '}';
+    }
 }
